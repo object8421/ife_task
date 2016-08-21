7c38065c62a779b9aff540266e1bde76c98ff47c
gaoqiang362 <gaoqiang362@gaoqiang362.(none)>
2016-08-21 17:42:40 +0800

    init ife task

---
+++ application/controllers/task/home.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class home extends MY_Controller {
+	function __construct() {
+		parent::__construct();
+	}
+
+	function index() {
+		$this->task1();
+	}
+	
+	
+	function task1() {
+		$this->load->view("task/task1_view");
+	}
+	
+
+}
+
---
+++ system/language/chinese/upload_lang.php
@@ -0,0 +1,25 @@
+<?php
+
+$lang['upload_userfile_not_set'] = "没有指定上传的用户文件, 上传失败!";
+$lang['upload_file_exceeds_limit'] = "上传的文件超过了系统允许的最大值, 上传失败!";
+$lang['upload_file_exceeds_form_limit'] = "上传的文件超过提交表单所允许的最大大小, 上传失败!";
+$lang['upload_file_partial'] = "该文件仅部分上传, 上传失败!";
+$lang['upload_no_temp_directory'] = "临时文件夹只是部分丢失, 上传失败！";
+$lang['upload_unable_to_write_file'] = "文件无法写入磁盘, 上传失败!";
+$lang['upload_stopped_by_extension'] = "文件上传被停止, 上传失败!";
+$lang['upload_no_file_selected'] = "你没有选择一个文件上传, 上传失败!";
+$lang['upload_invalid_filetype'] = "您要上传的文件类型是不允许的, 上传失败!";
+$lang['upload_invalid_filesize'] = " 你试图上传的文件大于允许的文件大小, 上传失败!";
+$lang['upload_invalid_dimensions'] = "你试图上传超过最大高度或宽度的图像, 上传失败!";
+$lang['upload_destination_error'] = "试图将上载文件移动到最终目的地时，遇到了一个问题, 上传失败!";
+$lang['upload_no_filepath'] = "上传路径不是有效的, 上传失败!";
+$lang['upload_no_file_types'] = "您没有指定任何允许的文件类型, 上传失败!";
+$lang['upload_bad_filename'] = "您提交的文件名已存在于服务器上, 上传失败!";
+$lang['upload_not_writable'] = "上传的目标文件夹不可写, 上传失败!";
+$lang['upload_same_filename'] = "服务器已存在相同名称的文件!";
+$lang['upload_fail'] = "上传失败!";
+$lang['upload_success'] = "上传成功!";
+$lang['upload_invalid_filename'] = '文件名只能由(数字|字母(a-zA-Z)|.|_组成)!';
+
+/* End of file upload_lang.php */
+/* Location: ./system/language/english/upload_lang.php */
